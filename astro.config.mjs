b719a3ce9109f618720d841e45aa00874145e4f1
Ariano2700 <kawuai123loco123@gmail.com>
2024-10-31 21:18:57 -0500

    add netflify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [tailwind()]
+  output: "server",
+  integrations: [tailwind()],
+  adapter: netlify(),
 });
