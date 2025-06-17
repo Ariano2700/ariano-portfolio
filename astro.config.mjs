931a6dd014d040cba55b7016ca289421750ad458
Ariano2700 <kawuai123loco123@gmail.com>
2025-06-17 18:47:16 -0500

    add: sitemap and robots.txt dynamic

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), react()],
+  site: "https://arianoalban.netlify.app/",
+  integrations: [tailwind(), react(), sitemap()],
   adapter: netlify(),
 });
